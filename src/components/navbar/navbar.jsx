07731a2f28662a7ae07615e72de313fb26b18717
Cayden-Covarrubias <113948748+Cayden-Covarrubias@users.noreply.github.com>
2022-10-26 08:06:16 -0700

    Add files via upload

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./navbar.css"
+
+export default function NavBar(){
+    return( 
+    <div className = "topNav">
+        <div className="topLeft"></div>
+        <div className="topCenter">
+        <ul className="topNavList">
+            <li className="navListItem Home">
+                Home
+            </li>
+            <li className="navListItem Home">
+                Home
+            </li>
+            <li className="navListItem Home">
+                Home
+            </li>
+            <li className="navListItem Home">
+                Home
+            </li>
+            <li className="navListItem Home">
+                Home
+            </li>
+            <li className="navListItem Home">
+                Home
+            </li>
+        </ul>
+        </div>
+        <div className="topRight"></div>
+
+    </div>)
+
+}
