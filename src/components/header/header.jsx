091223b642bc9e6656c37ffd3eb2ed38fcdbf655
cayden <cjcovarr@calpoly.edu>
2022-11-12 19:24:31 -0800

    Hey Im learning how to commit better

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
             <div classNmae="headerTitles" >
                 <span className="headerTitle2"> Connecting Pride </span>
                 <span className="headerTitle1"> Articles </span>
-                <img className="headerImg" src="images/background.png" alt="header" />
+                <img className="headerImg" src="images/background.jpg" alt="header" />
             </div>
         </div>
     )
