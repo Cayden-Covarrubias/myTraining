96c6e8241a29643a9fc92f5af41bf27c539ad4bd
cayden <cjcovarr@calpoly.edu>
2022-11-12 19:38:56 -0800

    Working on the posts functionality

---
+++ src/components/post/post.jsx
@@ -0,0 +1,21 @@
+import "./post.css"
+
+export default function Post(){
+
+return(
+    <div className="post">
+        <img className="postImage" src="/images/background.png"/>
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">cat 1 cat 2</span>
+
+            </div>
+            <span className="postTitle">title</span>
+            <hr />
+            <span className="postDate">today</span>
+        </div>
+        <p className="postSummary">Some words ........... some more words? </p>
+        </div>
+)
+
+}
