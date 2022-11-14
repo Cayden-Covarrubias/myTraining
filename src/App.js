bf99949babd7d82705454db86147a955d40eadfd
cayden <cjcovarr@calpoly.edu>
2022-11-14 11:47:23 -0800

    Completed writepostjsx

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import NavBar from './components/navbar/navbar'
 import CreateAccount from './pages/createAccount/createAccount';
 import Home from './pages/home/home'
 import PostPage from './pages/postPage/postPage'
+
+import WritePost from "./pages/writePost/writePost"
 import Login from './pages/login/login'
 import Settings from "./pages/settings/settings"
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Settings/>
+      <WritePost/>
     </div>
   );
 }
