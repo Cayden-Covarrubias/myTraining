575b801c85e8b82c4b7cf6f14323518b5aeb7a2d
cayden <cjcovarr@calpoly.edu>
2022-11-14 12:04:31 -0800

    Starting frontend routing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import PostPage from './pages/postPage/postPage'
 import WritePost from "./pages/writePost/writePost"
 import Login from './pages/login/login'
 import Settings from "./pages/settings/settings"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
 function App() {
+  const currentUser = false;
+  console.log(currentUser)
   return (
     <div className="App">
       <NavBar/>
